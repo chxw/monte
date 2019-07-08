f02e9cfd7fc741fa0c2410b96736662e221de84d
chxw <48259059+chxw@users.noreply.github.com>
2019-07-08 11:44:12 -0400

    Add files via upload

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+#!/Users/chelsea/practice/python/monte/venv python
+
 from flask import Flask, render_template, request, Response
 from graph_tools import *
 import urllib
@@ -16,12 +18,10 @@ import base64
 import re
 import wordninja
 
-from aylienapiclient import textapi
-import time
-import aylien_news_api
-from aylien_news_api.rest import ApiException
-
-from sqlalchemy import Column, Integer, String, PickleType
+# from aylienapiclient import textapi
+# import time
+# import aylien_news_api
+# from aylien_news_api.rest import ApiException
 
 from bokeh.io import curdoc
 from bokeh.models import HoverTool
@@ -36,14 +36,15 @@ import itertools
 
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 IEX_secret_token = os.getenv("IEX_secret_token")
 IEX_publishable_token = os.getenv("IEX_publishable_token")
 token = IEX_secret_token
 
-AYLIEN_key = os.getenv("AYLIEN_key")
-AYLIEN_id = os.getenv("AYLIEN_id")
+# AYLIEN_key = os.getenv("AYLIEN_key")
+# AYLIEN_id = os.getenv("AYLIEN_id")
 
 def camelcase_split(*args):
 	new_args =[]
@@ -104,7 +105,7 @@ class Asset():
 
 	def fetch_historicals(self, time_range):
 		query = urllib.parse.quote(self.ticker)
-		data = requests.get("https://cloud.iexapis.com/stable/stock/"+query+"/chart/"+time_range+"?token="+token+"&symbols="+query).json()
+		data = requests.get("https://cloud.iexapis.com/stable/stock/"+query+"/chart/"+time_range+"?token="+token).json()
 
 		# # dummy data
 		# data = [{"date":"2019-04-08","open":196.42,"close":200.1,"high":200.23,"low":196.34,"volume":25881697,"uOpen":196.42,"uClose":200.1,"uHigh":200.23,"uLow":196.34,"uVolume":25881697,"change":0,"changePercent":0,"label":"Apr 8","changeOverTime":0},{"date":"2019-04-09","open":200.32,"close":199.5,"high":202.85,"low":199.23,"volume":35768237,"uOpen":200.32,"uClose":199.5,"uHigh":202.85,"uLow":199.23,"uVolume":35768237,"change":-0.6,"changePercent":-0.2999,"label":"Apr 9","changeOverTime":-0.002999},{"date":"2019-04-10","open":198.68,"close":200.62,"high":200.74,"low":198.18,"volume":21695288,"uOpen":198.68,"uClose":200.62,"uHigh":200.74,"uLow":198.18,"uVolume":21695288,"change":1.12,"changePercent":0.5614,"label":"Apr 10","changeOverTime":0.002599},{"date":"2019-04-11","open":200.85,"close":198.95,"high":201,"low":198.44,"volume":20900808,"uOpen":200.85,"uClose":198.95,"uHigh":201,"uLow":198.44,"uVolume":20900808,"change":-1.67,"changePercent":-0.8324,"label":"Apr 11","changeOverTime":-0.005747},{"date":"2019-04-12","open":199.2,"close":198.87,"high":200.14,"low":196.21,"volume":27760668,"uOpen":199.2,"uClose":198.87,"uHigh":200.14,"uLow":196.21,"uVolume":27760668,"change":-0.08,"changePercent":-0.0402,"label":"Apr 12","changeOverTime":-0.006147},{"date":"2019-04-15","open":198.58,"close":199.23,"high":199.85,"low":198.01,"volume":17536646,"uOpen":198.58,"uClose":199.23,"uHigh":199.85,"uLow":198.01,"uVolume":17536646,"change":0.36,"changePercent":0.181,"label":"Apr 15","changeOverTime":-0.004348},{"date":"2019-04-16","open":199.46,"close":199.25,"high":201.37,"low":198.56,"volume":25696385,"uOpen":199.46,"uClose":199.25,"uHigh":201.37,"uLow":198.56,"uVolume":25696385,"change":0.02,"changePercent":0.01,"label":"Apr 16","changeOverTime":-0.004248},{"date":"2019-04-17","open":199.54,"close":203.13,"high":203.38,"low":198.61,"volume":28906780,"uOpen":199.54,"uClose":203.13,"uHigh":203.38,"uLow":198.61,"uVolume":28906780,"change":3.88,"changePercent":1.9473,"label":"Apr 17","changeOverTime":0.015142},{"date":"2019-04-18","open":203.12,"close":203.86,"high":204.15,"low":202.52,"volume":24195766,"uOpen":203.12,"uClose":203.86,"uHigh":204.15,"uLow":202.52,"uVolume":24195766,"change":0.73,"changePercent":0.3594,"label":"Apr 18","changeOverTime":0.018791},{"date":"2019-04-22","open":202.83,"close":204.53,"high":204.94,"low":202.34,"volume":19439545,"uOpen":202.83,"uClose":204.53,"uHigh":204.94,"uLow":202.34,"uVolume":19439545,"change":0.67,"changePercent":0.3287,"label":"Apr 22","changeOverTime":0.022139},{"date":"2019-04-23","open":204.43,"close":207.48,"high":207.75,"low":203.9,"volume":23322991,"uOpen":204.43,"uClose":207.48,"uHigh":207.75,"uLow":203.9,"uVolume":23322991,"change":2.95,"changePercent":1.4423,"label":"Apr 23","changeOverTime":0.036882},{"date":"2019-04-24","open":207.36,"close":207.16,"high":208.48,"low":207.05,"volume":17540609,"uOpen":207.36,"uClose":207.16,"uHigh":208.48,"uLow":207.05,"uVolume":17540609,"change":-0.32,"changePercent":-0.1542,"label":"Apr 24","changeOverTime":0.035282},{"date":"2019-04-25","open":206.83,"close":205.28,"high":207.76,"low":205.12,"volume":18543206,"uOpen":206.83,"uClose":205.28,"uHigh":207.76,"uLow":205.12,"uVolume":18543206,"change":-1.88,"changePercent":-0.9075,"label":"Apr 25","changeOverTime":0.025887},{"date":"2019-04-26","open":204.9,"close":204.3,"high":205,"low":202.12,"volume":18649102,"uOpen":204.9,"uClose":204.3,"uHigh":205,"uLow":202.12,"uVolume":18649102,"change":-0.98,"changePercent":-0.4774,"label":"Apr 26","changeOverTime":0.02099},{"date":"2019-04-29","open":204.4,"close":204.61,"high":205.97,"low":203.86,"volume":22204716,"uOpen":204.4,"uClose":204.61,"uHigh":205.97,"uLow":203.86,"uVolume":22204716,"change":0.31,"changePercent":0.1517,"label":"Apr 29","changeOverTime":0.022539},{"date":"2019-04-30","open":203.06,"close":200.67,"high":203.4,"low":199.11,"volume":46534923,"uOpen":203.06,"uClose":200.67,"uHigh":203.4,"uLow":199.11,"uVolume":46534923,"change":-3.94,"changePercent":-1.9256,"label":"Apr 30","changeOverTime":0.002849},{"date":"2019-05-01","open":209.88,"close":210.52,"high":215.31,"low":209.23,"volume":64827328,"uOpen":209.88,"uClose":210.52,"uHigh":215.31,"uLow":209.23,"uVolume":64827328,"change":9.85,"changePercent":4.9086,"label":"May 1","changeOverTime":0.052074},{"date":"2019-05-02","open":209.84,"close":209.15,"high":212.65,"low":208.13,"volume":31996324,"uOpen":209.84,"uClose":209.15,"uHigh":212.65,"uLow":208.13,"uVolume":31996324,"change":-1.37,"changePercent":-0.6508,"label":"May 2","changeOverTime":0.045227},{"date":"2019-05-03","open":210.89,"close":211.75,"high":211.84,"low":210.23,"volume":20892378,"uOpen":210.89,"uClose":211.75,"uHigh":211.84,"uLow":210.23,"uVolume":20892378,"change":2.6,"changePercent":1.2431,"label":"May 3","changeOverTime":0.058221},{"date":"2019-05-06","open":204.29,"close":208.48,"high":208.84,"low":203.5,"volume":32443113,"uOpen":204.29,"uClose":208.48,"uHigh":208.84,"uLow":203.5,"uVolume":32443113,"change":-3.27,"changePercent":-1.5443,"label":"May 6","changeOverTime":0.041879}]
